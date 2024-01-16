94b5ba6e630a6640ffcccee96c5be43601d1d698
Daniel Beene <57543540+dbeene@users.noreply.github.com>
2024-01-16 15:05:48 -0700

    General organization
    Refined messages/echo on code chunks - moved plotting function from RMD to R source file. Removed HTML output from repo.

--- plotting_functions.R
+++ plotting_functions.R
@@ -601,4 +601,34 @@ f_plot_corr_ellipse <- function(
   invisible()
 }
 
+# Plot.gwr.coefs is a general function for plotting GWR and MGWWR results from the GWModel package by Daniel Beene,
+# Department of Geography and Environmental Studies, University of New Mexico (dbeene@unm.edu).
+
+plot.gwr.coefs = function(gwr.model.SDF, variable.name, tvalues) {
+  # determine which observations are significant from via the t-values
+  tval = tvalues
+  insignif = tval >= -1.96 & tval <= 1.96
+  
+  # create the background and main layer with shaded polygons
+  p = tm_shape(gwr.model.SDF) +
+    tm_polygons(variable.name, palette = "RdYlBu", title = variable.name, alpha = 0.7) +
+    tm_borders(lwd = 0.5) +  # Add borders for all polygons
+    
+    tm_layout(legend.outside = TRUE,  # Place legend outside the plot
+              legend.position = c("right", "top"),  # Position the legend to the right
+              legend.outside.size = 0.3,  # Adjust the legend width as needed
+    )  
+  
+  # check if there are any non-empty units for the insignificant observations
+  if (any(insignif)) {
+    # now add the t-values layer
+    p = p +
+      tm_shape(gwr.model.SDF[insignif,]) +  # Use insignif for insignificant observations
+      tm_polygons("black", alpha = 0.75)
+  }
+  
+  return(p)
+}
+
+
 # EOF
